065d56fd09ebe932e9c8ca3e0c46c556ebb44e79
nikolin-anton <antonnikolin@gmail.com>
2023-11-08 16:44:03 +0200

    add user and blog seeder

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $is_published = rand(0,1);
+        return [
+            'title' => $this->faker->sentence(rand(3,8),true),
+            'content' => $this->faker->realText(rand(1000,4000)),
+            'is_published' => $is_published,
+            'published_at' => $is_published === 1
+                ? $this->faker->dateTimeBetween('-1 week', '+1 week') : null,
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,10 +21,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->unique()->email,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('12345678'),
             'remember_token' => Str::random(10),
         ];
     }
